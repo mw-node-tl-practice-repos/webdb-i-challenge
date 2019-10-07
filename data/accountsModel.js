ac44d80bcc5987350a54315526b21dbddf669da5
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-10-06 22:27:41 -0700

    Updated more things

---
+++ data/accountsModel.js
@@ -0,0 +1,39 @@
+const db = require('./data/dbConfig.js');
+
+module.exports = {
+    find,
+    findById,
+    insert,
+    update,
+    remove
+}
+
+function find() {
+    return db('accounts');
+}
+
+function findById(id) {
+    return db('accounts')
+    .where({id})
+    .first();
+}
+
+function insert(account) {
+    return db('accounts')
+    .insert(account)
+    .then(items => {
+        return findById(items[0])
+    });
+}
+
+function update(id, changes) {
+    return db('accounts')
+    .where({id})
+    .update(changes);
+}
+
+function remove(id) {
+    return db('accounts')
+    .where('id', id)
+    .del();
+}
